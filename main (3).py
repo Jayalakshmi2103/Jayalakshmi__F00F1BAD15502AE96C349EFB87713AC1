d54eb9f708f9abd471fcd27f91698e218de3a236
Jayalakshmi2103 <144470588+Jayalakshmi2103@users.noreply.github.com>
2023-09-26 21:04:48 +0530

    Add files via upload

--- main (3).py
+++ main (3).py
@@ -1,20 +1,17 @@
-#Implement a class called Player and derive two classes called Batsman,Bowler.
-
-class Player:
-  def play(self):
-      print("The player is playing cricket.")
-
-class Batsman(Player):
-  def play(self):
-      print("The batsman is batting.")
-
-class Bowler(Player):
-  def play(self):
-      print("The bowler is bowling.")
-
-batsman = Batsman()
-bowler = Bowler()
-
-#Calling the play() method for each object.
-batsman.play()
-bowler.play()
+class students:
+  def __init__(self,name,rn,cgpa):
+    self.name=name
+    self.rn=rn
+    self.cgpa=cgpa
+def sort_students(student_list):
+  sorted_students=sorted(student_list,key=lambda student:student.cgpa,reverse=True)
+  return sorted_students
+students=[
+  students("Sam","A111",7.8),
+  students("Jam","A112",8.9),
+  students("Ram","A113",9.1),
+  students("Cam","A114",9.9)
+]
+sorted_students=sort_students(students)
+for student in sorted_students:
+  print("Name: {},RollNO: {},CGPA: {}".format(student.name,student.rn,student.cgpa))
